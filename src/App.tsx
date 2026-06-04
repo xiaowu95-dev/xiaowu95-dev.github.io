dc8affc7d219c589445ad7c355512cfdcb7f3989
zi.b.wang <zi.b.wang@rakuten.com>
2026-06-04 14:12:06 +0800

    Add Meiqing page route and update font styles; include GSAP dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import AppDetail from '@/pages/AppDetail'
 import Home from '@/pages/Home'
+import Meiqing from '@/pages/Meiqing'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/app/kogo" element={<AppDetail />} />
+      <Route path="/meiqing" element={<Meiqing />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
